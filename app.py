1b5479af66fad6b62de2fd5d6a3e64c7bc83a1b5
Joseph Cox <josephcox@USF-Gold-Wifi-226-61-5.laptops.usf.edu>
2019-09-04 17:37:29 -0400

    set track modifications to false

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 api = Api(app)
 
