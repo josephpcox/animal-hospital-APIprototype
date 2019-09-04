790ebb9476617a460e90987bc7f5fc4bb95c6042
Joseph Cox <josephcox@USF-Gold-Wifi-226-5-245.laptops.usf.edu>
2019-09-04 15:48:07 -0400

    fixed api call

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
-api = API(app)
+api = Api(app)
 
 # Create users table and the users database model inhereted by sqlalchemy
 class Users(db.Model):
