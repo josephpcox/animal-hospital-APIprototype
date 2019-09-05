e1b063ecc8b022aafe48579960167a9e1cb60a75
Joseph Cox <josephcox@Josephs-MacBook-Air.local>
2019-09-05 04:18:37 -0400

    removed admin bool flag from code base for testing

--- app.py
+++ app.py
@@ -24,9 +24,8 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     first_name = db.Column(db.String(50),nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False,unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
-    admin = db.Column(db.Boolean(), nullable=True)
 
     # Users constructor
     def __init__self(self, id, first_name, last_name, email, password):
@@ -34,7 +33,6 @@ class Users(db.Model):
         self.last_name = last_name
         self.email = email
         self.password = password
-        self.admin = False
 
     @classmethod
     def find_by_email(cls, email):
